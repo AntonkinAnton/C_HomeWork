1872c9cb992a357950a22d0dd9d707cc6aa65d7e
AntonAntonkin <antonkin_anton@mail.ru>
2022-12-17 00:22:38 +0500

    initial commit + first version of task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//ждем ввод двух чисел, сверяем одно с другим, выводим макс.
+Console.Clear();
+Console.WriteLine("Антонкин Антон | Группа Разработчик 3776 | GeekBrains");
+Console.WriteLine(" ");
+Console.WriteLine("Программа показывает большее из двух чисел");
+Console.WriteLine(" ");
+Console.WriteLine("Введите первое число:");
+
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+if (num1 > num2)
+{
+    Console.WriteLine("Число {0} больше", num1);
+}
+else if (num1 == num2)
+{
+     Console.WriteLine("Числа равны!");
+}
+else
+{
+    Console.WriteLine("Число {0} больше", num2);
+}
+Console.ReadKey();
