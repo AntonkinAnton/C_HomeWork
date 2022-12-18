2898d61e5ce80bb8c39bf70a5fef4cdd9cbb369c
AntonAntonkin <antonkin_anton@mail.ru>
2022-12-18 18:37:06 +0500

    task 4 complete v.0.1

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//mod: Занести значения в List
+//получаем число N, начинаем перебор четных от 1 с внесением в List, с отсечкой по N
+
+Console.Clear();
+Console.WriteLine("Антонкин Антон | Группа Разработчик 3776 | GeekBrains");
+Console.WriteLine();
+Console.WriteLine("Программа показывает все четные числа от 1 до введенного числа");
+Console.WriteLine();
+Console.WriteLine("Введите число:");
+Console.WriteLine();
+
+
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+List<int> Numbers = new List<int>();
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        Numbers.Add(count);
+    }
+    count++;
+}
+
+
+Console.WriteLine(string.Join(":", Numbers));
