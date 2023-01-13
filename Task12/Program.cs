021986944b7e20410f450cf56835a2996972d040
AntonAntonkin <antonkin_anton@mail.ru>
2023-01-13 22:01:37 +0500

    Task 12 - 3in1/ Task13 - Pow

---
+++ Task12/Program.cs
@@ -0,0 +1,55 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму, произведение чисел от 1 до А.
+void Setcolor(int clr)
+
+{
+
+    if (clr == 1) Console.ForegroundColor = ConsoleColor.White;
+    else if (clr == 2) Console.ForegroundColor = ConsoleColor.Blue;
+    else if (clr == 3) Console.ForegroundColor = ConsoleColor.Red;
+
+}
+int A;
+int sum = 0;
+long mult = 1;
+int flag = 1;
+bool show=false;
+ConsoleKeyInfo Key;
+do
+{
+    Console.Clear();
+    Console.WriteLine("Программа показывает сумму и произведение чисел от 1 до A");
+    Console.WriteLine();
+    Console.WriteLine("Введите число:");
+} while (!int.TryParse(Console.ReadLine(), out A));
+
+do
+{
+    Console.WriteLine();
+    Console.WriteLine($"Показать все числа от 1 до {A}? Y/N");
+    Key = Console.ReadKey();
+} while (Key.Key != ConsoleKey.Y && Key.Key != ConsoleKey.N);
+if (Key.Key == ConsoleKey.N) show = false;
+if (Key.Key == ConsoleKey.Y) show = true;
+Console.WriteLine();
+for (int i = 1; i <= A; i++, flag++)
+{
+    if (show)
+    {
+    Setcolor(flag);
+    Console.Write(i);
+    Console.Write(" ");
+    }
+    sum += i;
+    mult *= i;
+    flag %= 3;
+}
+Console.WriteLine();
+Console.WriteLine();
+Console.ResetColor();
+Console.WriteLine($"Сумма = {sum}");
+
+Console.WriteLine(mult == 0 && A > 0 ? "Произведение больше чем 9223372036854775807" : "Произведение = " + mult);
+
+Console.WriteLine("Количество цифр числа " + A + " = " + ((int)Math.Log10(A) + 1));
+Console.ReadKey();
+
---
+++ Task13/Program.cs
@@ -0,0 +1,70 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+
+//Сказано написать цикл, значит готовый метод Math.Pow не подойдет. Напишем свой ¯\_(ツ)_/¯
+
+int InputNum(string message)
+{
+    int num;
+    do
+    {
+        System.Console.WriteLine(message);
+    }
+    while (!int.TryParse(Console.ReadLine(), out num));
+    return num;
+}
+
+void MyPow(int num, int degree)
+{
+    int result = 1;
+    for (int i = 1; i <= degree; i++)
+        result *= num;
+    CW(num, degree, result);
+}
+
+string ToUpperIndex(int degree)
+{
+    string UpIndex = degree.ToString();
+    string result = String.Empty;
+    for (int i = 0; i < UpIndex.Length; i++)
+    {
+        if (UpIndex[i] == '0') result += '⁰';
+        if (UpIndex[i] == '1') result += '¹';
+        if (UpIndex[i] == '2') result += '²';
+        if (UpIndex[i] == '3') result += '³';
+        if (UpIndex[i] == '4') result += '⁴';
+        if (UpIndex[i] == '5') result += '⁵';
+        if (UpIndex[i] == '6') result += '⁶';
+        if (UpIndex[i] == '7') result += '⁷';
+        if (UpIndex[i] == '8') result += '⁸';
+        if (UpIndex[i] == '9') result += '⁹';
+
+    }
+    return result;
+
+}
+
+void CW(int num, int degree, int result)
+{
+    System.Console.WriteLine();
+    System.Console.WriteLine($"{num}{ToUpperIndex(degree)} = {result}");
+}
+
+void Header()
+{
+    Console.Clear();
+    Console.WriteLine("Антонкин Антон | Группа Разработчик 3776 | GeekBrains");
+    Console.WriteLine();
+    Console.WriteLine("Программа возводит указанное число в указанную степень");
+    Console.WriteLine();
+
+}
+
+Header();
+
+int num = InputNum("Введите число:");
+int degree = InputNum("Введите степень:");
+
+MyPow(num, degree);
+
+Console.ReadKey();
