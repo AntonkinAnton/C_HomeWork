eb9185e21243aedffddf0e4133a21abf70bcf5d1
AntonAntonkin <antonkin_anton@mail.ru>
2023-01-28 21:48:11 +0500

    spiral task

---
+++ Task27/Program.cs
@@ -0,0 +1,181 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+
+void Header(string about)
+{
+    Console.Clear();
+    Console.WriteLine("Антонкин Антон | Группа Разработчик 3776 | GeekBrains \n");
+    Console.WriteLine($"{about}\n");
+}
+
+int GetNum(string message)
+{
+    int num;
+    do
+    {
+        System.Console.WriteLine(message);
+    } while (!int.TryParse(Console.ReadLine(), out num));
+    System.Console.WriteLine();
+    return num;
+}
+
+void PrintImage(int[,] array, string message)
+{
+    System.Console.WriteLine(message);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            System.Console.Write(array[i, j] + "\t");
+
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[,] SpiralArray(int size) 
+{
+    int[,] array = new int[size, size];
+
+    int pos = 1;
+    int sum = -1;
+    int count = size;
+    int value = -size;
+    
+    do 
+    {
+        value = -1 * value / size;
+        for (int i = 0; i < count; i++) 
+        {
+            sum += value;
+            array[sum / size, sum % size] = pos++;
+        }
+        value *= size;
+        count--;
+        for (int i = 0; i < count; i++)
+         {
+            sum += value;
+            array[sum / size, sum % size] = pos++;
+        }
+    } while (count > 0);
+
+    return array;
+}
+
+void SetFontColor(int clr)
+{
+    switch (clr)
+    {
+
+        case 1:
+            Console.ForegroundColor = ConsoleColor.Blue;
+            break;
+        case 2:
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            break;
+        case 3:
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            break;
+        case 4:
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            break;
+        case 5:
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            break;
+        case 6:
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            break;
+        case 7:
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            break;
+        case 8:
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            break;
+        case 9:
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            break;
+        case 10:
+            Console.ForegroundColor = ConsoleColor.Gray;
+            break;
+        case 11:
+            Console.ForegroundColor = ConsoleColor.Green;
+            break;
+        case 12:
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            break;
+        case 13:
+            Console.ForegroundColor = ConsoleColor.Red;
+            break;
+        case 14:
+            Console.ForegroundColor = ConsoleColor.White;
+            break;
+        case 15:
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            break;
+
+    }
+}
+
+async void RunningString(string endmessage, int delayMs)
+{
+    Console.CursorVisible = false;
+    endmessage += " ";
+    Random rnd = new Random();
+
+    while (true)
+    {
+        for (int i = 0; i < endmessage.Length; i++)
+        {
+            Console.SetCursorPosition(endmessage.Length - 1 - i, Console.CursorTop);
+            for (int j = 0; j <= i; j++)
+            {
+                System.Console.Write(endmessage[j]);
+            }
+
+            await Task.Delay(delayMs);
+
+            if (i == endmessage.Length - 1)
+            {
+                for (int i1 = 0; i1 < endmessage.Length; i1++)
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop);
+                    for (int j1 = i1 + 1; j1 < endmessage.Length; j1++)
+                    {
+                        System.Console.Write(endmessage[j1]);
+                    }
+                    await Task.Delay(delayMs);
+                }
+
+                SetFontColor(rnd.Next(1, 16));
+            }
+
+        }
+    }
+}
+
+void End()
+{
+    Console.ReadKey();
+    return;
+}
+
+
+Header("Программа рисует массив по спирали");
+
+int arraySize = GetNum("Введите длинну стороны:");
+
+int[,] array = SpiralArray(arraySize); 
+
+PrintImage(array, "Ваш рисунок:");
+
+RunningString("Нажмите любую клавишу, чтобы выйти... ", 70);
+
+End();
